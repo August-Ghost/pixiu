51552dcfebdd0209fe60f5b8df18ca8b8e7f7a79
Ghost <archangel_august@gmail.com>
2017-07-13 08:37:43 +0800

    KDJ bug fix: j = 3k-2d

--- index/kdj.py
+++ index/kdj.py
@@ -24,7 +24,7 @@ class KDJ:
                 self.cache["id"] = identifier
             self.cache["k"] = (2 / self.m2) * self._k + (1 / self.m1) * rsv
             self.cache["d"] = (2 / self.m2) * self._d + (1 / self.m1) * self.cache["k"]
-            self.cache["j"] = 3 * self.cache["d"] - 2 * self.cache["k"]
+            self.cache["j"] = 3 * self.cache["k"] - 2 * self.cache["d"]
         return {"id": identifier,
                 "rsv": rsv,
                 "k": self.cache["k"],
